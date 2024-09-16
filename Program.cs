2007b846a0f7557842ae2ef113616e0e96e7d2a9
Danny Lynch <dannysfolder@Dannys-MacBook-Pro.local>
2024-09-16 12:03:54 -0500

    Lab 3

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿string enjoymentLevel = GetEnjoymentLevel();
+string stadiumrecommendation = GetStadiumRecommendation(enjoymentLevel);
+string gamerecommendation = GetGameRecommendation(stadiumrecommendation);
+DisplayStadiumDetails(stadiumrecommendation,gamerecommendation);
+
+static string GetEnjoymentLevel(){
+    System.Console.WriteLine("What enjoyment level would you like to attend?");
+    return Console.ReadLine();
+}
+
+static string GetStadiumRecommendation(string enjoymentLevel){
+    if (enjoymentLevel == "Boring"){
+        return "Neyland Stadium";
+    }
+    else if(enjoymentLevel == "Average"){
+        return "Jordan-Hare Stadium";
+    }
+    else if(enjoymentLevel == "Fun"){
+        return "Tiger Stadium";
+    }
+    else if(enjoymentLevel == "Epic"){
+        return "Sabam Field at Bryant-Denny Stadium"; 
+    }
+    else return "Wrong input";
+
+}
+static string GetGameRecommendation(string stadium){
+     if( stadium == "Neyland Stadium"){
+          return " vs Kent State";
+     }
+     else if( stadium == "Jordan-Hare Stadium"){
+         return " vs Kentucky";
+      }
+      else if( stadium == "Tiger Stadium"){
+          return " vs Alabama";
+      }
+      else if( stadium == "Sabam Field at Bryant-Denny Stadium"){
+          return " vs Auburn";
+
+        }
+    else return "Wrong Input";
+}
+static void DisplayStadiumDetails(string stadium, string game){
+    Console.WriteLine( stadium + game);
+}
